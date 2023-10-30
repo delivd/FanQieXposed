34de040ecd52c9fde3bc4f7bb21ac3127e41967c
huxing <huxing@libii.com>
2023-10-30 18:11:46 +0800

    适配最新版本5.9.9.32

--- app/src/main/java/com/hx/fanqie/AppHook.kt
+++ app/src/main/java/com/hx/fanqie/AppHook.kt
@@ -1,6 +1,7 @@
 package com.hx.fanqie
 
 import android.R.attr.classLoader
+import android.util.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -14,43 +15,106 @@ class AppHook : IXposedHookLoadPackage {
         if (lpparam.packageName == "com.dragon.read") {
             XposedBridge.log("start hook...")
             /**
-             * 去掉广告
+             * 去掉广告 5.9.5版本去广告
              */
-            XposedHelpers.findAndHookMethod("com.dragon.read.component.biz.impl.g.e",
-                lpparam.classLoader,
-                "isNoAd",
-                String::class.java,
-                object : XC_MethodHook() {
-                    @Throws(Throwable::class)
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        super.beforeHookedMethod(param)
-                    }
 
-                    @Throws(Throwable::class)
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        super.afterHookedMethod(param)
-                        param.result = true
-                    }
-                })
+            try {
+                lpparam.classLoader.loadClass("com.dragon.read.component.biz.impl.g.e")
+                XposedHelpers.findAndHookMethod("com.dragon.read.component.biz.impl.g.e",
+                    lpparam.classLoader,
+                    "isNoAd",
+                    String::class.java,
+                    object : XC_MethodHook() {
+                        @Throws(Throwable::class)
+                        override fun beforeHookedMethod(param: MethodHookParam) {
+                            super.beforeHookedMethod(param)
+                        }
+
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            super.afterHookedMethod(param)
+                            param.result = true
+                        }
+                    })
+            } catch (e: Exception) {
+                XposedBridge.log("com.dragon.read.component.biz.impl.g.e:${e.message}")
+            }
             /**
-             * 去掉更新
+             * 5.9.5 去掉更新
              */
-            XposedHelpers.findAndHookMethod(
-                "com.dragon.read.update.d",
-                lpparam.classLoader,
-                "b",
-                object : XC_MethodHook() {
-                    @Throws(Throwable::class)
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        super.beforeHookedMethod(param)
-                    }
 
-                    @Throws(Throwable::class)
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        super.afterHookedMethod(param)
-                        param.result = null
-                    }
-                })
+
+            try {
+                lpparam.classLoader.loadClass("com.dragon.read.update.d")
+                XposedHelpers.findAndHookMethod(
+                    "com.dragon.read.update.d",
+                    lpparam.classLoader,
+                    "b",
+                    object : XC_MethodHook() {
+                        @Throws(Throwable::class)
+                        override fun beforeHookedMethod(param: MethodHookParam) {
+                            super.beforeHookedMethod(param)
+                        }
+
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            super.afterHookedMethod(param)
+                            param.result = null
+                        }
+                    })
+            } catch (e: Exception) {
+                XposedBridge.log("com.dragon.read.update.d:${e.message}")
+
+            }
+
+//            5.9.9版本去广告
+            try {
+                lpparam.classLoader.loadClass("com.dragon.read.component.biz.impl.h.e")
+                XposedHelpers.findAndHookMethod("com.dragon.read.component.biz.impl.h.e",
+                    lpparam.classLoader,
+                    "isNoAd",
+                    String::class.java,
+                    object : XC_MethodHook() {
+                        @Throws(Throwable::class)
+                        override fun beforeHookedMethod(param: MethodHookParam) {
+                            super.beforeHookedMethod(param)
+                        }
+
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            super.afterHookedMethod(param)
+                            param.result = true
+                        }
+                    })
+            } catch (e: Exception) {
+                XposedBridge.log("com.dragon.read.component.biz.impl.h.e:${e.message}")
+
+            }
+//            5.9.9版本去更新
+            try {
+                lpparam.classLoader.loadClass("com.dragon.read.update.UpdateManager")
+                XposedHelpers.findAndHookMethod(
+                    "com.dragon.read.update.UpdateManager",
+                    lpparam.classLoader,
+                    "b",
+                    object : XC_MethodHook() {
+                        @Throws(Throwable::class)
+                        override fun beforeHookedMethod(param: MethodHookParam) {
+                            super.beforeHookedMethod(param)
+                        }
+
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            super.afterHookedMethod(param)
+                            param.result = null
+                        }
+                    })
+            } catch (e: Exception) {
+                XposedBridge.log("com.dragon.read.update.UpdateManager:${e.message}")
+
+            }
+
+
         }
     }
 }
